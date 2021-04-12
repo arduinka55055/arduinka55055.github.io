ea6c53e7f017a1911ade0347a1195577af768e24
Denis <denistesla2005@gmail.com>
2021-04-12 13:28:53 +0300

    !

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-document.querySelector("body").style.overflowX="hidden";
-document.querySelector("body").style.overflowY="hidden";
+document.body.style.overflowX="hidden";
+document.body.style.overflowY="hidden";
 
 document.addEventListener("DOMContentLoaded", () => {
     window.scrollTo(0, 0);
 
-    document.querySelector(".banner").addEventListener("animationend", () => { document.querySelector("body").style.overflowY=""});
+    document.querySelector(".banner").addEventListener("animationend", () => { document.body.style.overflowY=""});
 });
