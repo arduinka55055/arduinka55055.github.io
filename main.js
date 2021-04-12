8bf0e8bae0b35fec522d6f61e066fef909732438
Denis <denistesla2005@gmail.com>
2021-04-12 13:39:00 +0300

    наконец то!

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-document.body.style.overflowX="hidden";
-document.body.style.overflowY="hidden";
 
 document.addEventListener("DOMContentLoaded", () => {
     window.scrollTo(0, 0);
 
-    document.querySelector(".banner").addEventListener("animationend", () => { document.body.style.overflowY=""});
+    document.querySelector(".banner").addEventListener("animationend", () => { document.body.setAttribute("class","done")});
 });
