752ca3be6b748808e03d30905f31cfb79a5cded3
Denis <denistesla2005@gmail.com>
2021-04-12 13:27:29 +0300

    test it

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+document.querySelector("body").style.overflowX="hidden"
+document.querySelector("body").style.overflowY="hidden"
+
 document.addEventListener("DOMContentLoaded", () => {
     window.scrollTo(0, 0);
 
-    document.querySelector(".banner").addEventListener("animationend", () => { blocker = true; });
-    
-
+    document.querySelector(".banner").addEventListener("animationend", () => { document.querySelector("body").style.overflowY=""});
 });
